b81a47053e2988611e8ac2c440656d41a31f97c9
Ivan Šmuk <ivanikismuk@gmail.com>
2023-12-03 19:51:21 +0100

    1

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="main">
+    <div class="navbar">     
+        <div class="logo-image">
+            <img src="images/logo.png" alt="lopta logo">
+        </div>   
+        <ul>
+            <a href=""> <li> Početna </li> </a>
+            <a href=""> <li> Fudbal </li> </a>
+            <a href=""> <li> Odbojka </li> </a>
+            <a href=""> <li> Košarka </li> </a>
+        </ul>
+    </div>
+
+    <br><br><br><br><br><br><br><br>
+    <h1>Dobrodošli u našu prodavnicu lopti!</h1>
+
+    <form action=""></form>
+</div>
+</body>
+</html>
